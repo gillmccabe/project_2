c71597826db495fbf0c3d9fcaed57fc790f5d6ec
Gillian McCabe <gillianmccabe@outlook.com>
2016-09-05 18:47:43 +0100

    main activity class set up

--- app/src/main/java/com/example/user/todolistnew/MainActivity.java
+++ app/src/main/java/com/example/user/todolistnew/MainActivity.java
@@ -1,7 +1,20 @@
 package com.example.user.todolistnew;
 
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
 /**
  * Created by user on 05/09/2016.
  */
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
 }
