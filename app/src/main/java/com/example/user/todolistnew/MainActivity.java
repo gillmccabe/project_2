8f5ab7885389fb1698bb2517927604b77626c157
Gillian McCabe <gillianmccabe@outlook.com>
2016-09-07 17:25:45 +0100

    updated main activity layout

--- app/src/main/java/com/example/user/todolistnew/MainActivity.java
+++ app/src/main/java/com/example/user/todolistnew/MainActivity.java
@@ -89,27 +89,29 @@ public class MainActivity extends AppCompatActivity {
 
         final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
         alertDialogBuilder.setTitle("Add Item To List");
-        alertDialogBuilder.setMessage("Are You Sure ?")
-                .setCancelable(false)
-                .setPositiveButton("Add Item", new DialogInterface.OnClickListener(){
-                    public void onClick(DialogInterface dialog, int id){
-
+        alertDialogBuilder.setMessage("Are You Sure ?");
+        alertDialogBuilder.setCancelable(true);
+        alertDialogBuilder.setPositiveButton("Add Item", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+//
 //        final EditText nameText = (EditText) alert.findViewById(R.id.name);
 //        Button addBtn = (Button) alert.findViewById(R.id.dialogButtonOK);
 //
 //        addBtn.setOnClickListener(new View.OnClickListener() {
 //            public void onClick(View view) {
-                String itemName = nameText.getText().toString();
-                ToDoItem item = new ToDoItem();
-                item.setName(itemName);
+//                String itemName = nameText.getText().toString();
+//                ToDoItem item = new ToDoItem();
+//                item.setName(itemName);
 //
-                myToDoItemAdapter.add(item); // ADD NEW TODOITEM TO ADAPTER
-                submitText.setText(""); // SET TEXT ENTERED INTO EDITTEXT FIELD
-                writeItems(item); //SAVE INTO DATABASE
+//                myToDoItemAdapter.add(item); // ADD NEW TODOITEM TO ADAPTER
+//                submitText.setText(""); // SET TEXT ENTERED INTO EDITTEXT FIELD
+//                writeItems(item); //SAVE INTO DATABASE
 //            }
 //        });
-                        AlertDialog alertDialog = alertDialogBuilder.create();
-                        alertDialog.show();
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
+        });
     }
 
 
